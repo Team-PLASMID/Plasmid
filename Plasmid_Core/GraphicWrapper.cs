e4e60e795e7e2d7a5d416631e0793da300748912
Cody Brock <crbrock@asu.edu>
2021-03-17 20:28:45 -0700

    Modify GraphicWrapper

--- Plasmid_Core/GraphicWrapper.cs
+++ Plasmid_Core/GraphicWrapper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,27 @@ namespace Plasmid_Core
 {
     class GraphicWrapper
     {
+        private GraphicsDeviceManager Graphics;
+        private int BaseHeight;
+        private int BaseWidth;
+
+        private Matrix ScaleMatrix;
+
+        public GraphicWrapper(GraphicsDeviceManager graphics, int h, int w )
+        {
+            Graphics = graphics;
+            BaseHeight = h;
+            BaseWidth = w;
+
+            // *TODO*
+            // Make this calculate scale
+
+            Graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+            Graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            Graphics.IsFullScreen = true;
+            Graphics.ApplyChanges();
+            ScaleMatrix = Matrix.CreateScale(2);
+        }
+
     }
 }
