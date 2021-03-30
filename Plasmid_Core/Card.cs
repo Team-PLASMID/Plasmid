e523d1a18a015a2d74031f144ed9d3a249075735
Cody Brock <crbrock@asu.edu>
2021-03-29 18:41:38 -0700

    Some placeholder art, card sprite assembly

--- Plasmid_Core/Card.cs
+++ Plasmid_Core/Card.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.Xna.Framework.Content;
+using System.Diagnostics;
 
 namespace Plasmid_Core
 {
@@ -12,17 +13,22 @@ namespace Plasmid_Core
         public static int Height = 96;
         public static int Width = 64;
         public static Color CardColor = Color.OldLace;
+        public static Color CardFrameColor = Color.DarkGoldenrod;
         public static Texture2D CardTexture;
+        public static Texture2D CardFrameTexture;
+
         public string Name { get; }
         public string Text { get; }
         public string Art { get; }
-        public RenderTarget2D Sprite { get; set; }
+        public RenderTarget2D Texture { get; set; }
         public int CostA { get; set; }
         public int CostG { get; set; }
         public int CostT { get; set; }
         public int CostC { get; set; }
         public Card Above { get; set; }
         public Card Below { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
 
         public Card(string name, string text, string art, int a, int g, int t, int c)
         {
@@ -38,20 +44,26 @@ namespace Plasmid_Core
             Below = null;
         }
 
-        public static void LoadTextures(ContentManager Content)
+        public static void LoadTextures(ContentManager content)
         {
-            CardTexture = Content.Load<Texture2D>("card");
+            CardTexture = content.Load<Texture2D>("card");
+            CardFrameTexture = content.Load<Texture2D>("card_frame");
         }
 
-        public void BuildSprite(GraphicsDevice graphics, SpriteBatch sb)
+        public void BuildTexture(ContentManager content, GraphicsDevice graphics, SpriteBatch sb)
         {
-            Sprite = new RenderTarget2D(graphics, CardTexture.Width, CardTexture.Height);
-            graphics.SetRenderTarget(Sprite);
+            Debug.WriteLine("\n\n BUILD SPRITE \n\n");
+            Texture = new RenderTarget2D(graphics, CardTexture.Width, CardTexture.Height);
+            graphics.SetRenderTarget(Texture);
             graphics.Clear(Color.Transparent);
+
+            Texture2D artTexture = content.Load<Texture2D>(Art);
 
             sb.Begin(SpriteSortMode.Immediate, null, SamplerState.PointClamp, null, null, null, Matrix.CreateScale(1));
 
-            //sb.Draw(CardTexture, new Vector2(0, 0), CardColor);
+            sb.Draw(CardTexture, Vector2.Zero, CardColor);
+            sb.Draw(artTexture, new Rectangle(3,3,58,58), Color.White);
+            sb.Draw(CardFrameTexture, Vector2.Zero, CardFrameColor);
 
             // Name
             // Art
@@ -65,4 +77,6 @@ namespace Plasmid_Core
         }
 
     }
+
+
 }
--- Plasmid_Core/Game1.cs
+++ Plasmid_Core/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Plasmid_Core
 {
@@ -8,26 +10,45 @@ namespace Plasmid_Core
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Matrix ScaleMatrix;
+
+        private List<Card> CardList;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+            CardList = new List<Card>();
+
+            CardList.Add(new Card("Bulbasaur", "Seed Pokemon", "bulb", 1, 1, 1, 1));
+            CardList.Add(new Card("Charmander", "Lizard Pokemon", "char", 1, 1, 1, 1));
+            CardList.Add(new Card("Squirtle", "Tiny Turtle Pokemon", "squirt", 1, 1, 1, 1));
+            Debug.WriteLine("MADE " + CardList + " CARDS.");
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            // TODO: Real dynamic scaling
+            ScaleMatrix = Matrix.CreateScale(4);
 
             base.Initialize();
+
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            Card.LoadTextures(Content);
+            foreach (Card card in CardList)
+                card.BuildTexture(Content, GraphicsDevice, _spriteBatch);
+
+            Debug.WriteLine("LOADED " + CardList + " CARDS.");
+            foreach (Card card in CardList)
+                Debug.WriteLine(card.Name + "   texture null:" + (card.Texture == null));
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,16 +56,26 @@ namespace Plasmid_Core
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
-
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin(SpriteSortMode.Immediate, null, null, null, null, null, ScaleMatrix);
+
+            int x = 20;
+            int y = 20;
+            foreach (Card card in CardList)
+            {
+                if (card.Texture == null)
+                    continue;
+                _spriteBatch.Draw(card.Texture, new Vector2(x, y), Color.White);
+                x += 80;
+            }
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
